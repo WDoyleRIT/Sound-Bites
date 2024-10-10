899de19700c07125a4cae306a20e746ba47095b6
Joseph <jws8554@rit.edu>
2024-10-10 12:25:19 -0400

    Room camera controller in my scene

---
+++ Roasted Beats/Assets/Scripts/Level/CameraCon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class CameraCon : MonoBehaviour
+{
+    public UnityEvent<Vector3> CameraEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = GetMouseScreenPos();
+        offset.z = 0;
+
+        // Clamps and inverts values
+        offset.x = -Mathf.Clamp(offset.x, -.5f, .5f);
+        offset.y = -Mathf.Clamp(offset.y, -.5f, .5f);
+
+        // Sends offset off to the room layout
+        CameraEvent.Invoke(offset);
+    }
+
+    private Vector3 GetMouseScreenPos()
+    {
+        Vector2 offset = Input.mousePosition;
+
+        // Returns a value between -.5f and .5f for x and y based on where mouse is 
+
+        offset = new Vector2 ((offset.x - Screen.width / 2) / Screen.width, (offset.y - Screen.height / 2) / Screen.height);
+
+        //Debug.Log(offset);
+        return offset;
+    }
+}
--- Roasted Beats/Assets/Scripts/Level/RoomLayout.cs
+++ Roasted Beats/Assets/Scripts/Level/RoomLayout.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RoomLayout : MonoBehaviour
 {
     [SerializeField] private List<GameObject> roomLayers;
 
     [SerializeField] private float layerOffset;
+    [SerializeField] private float roomOffsetMax;
+
+    private Vector3 cameraOffset;
 
     private List<GameObject> instantiatedLayers;
 
@@ -23,6 +27,21 @@ public class RoomLayout : MonoBehaviour
     private void Start()
     {
         GenerateLayers();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < instantiatedLayers.Count; i++)
+        {
+            GameObject layer = instantiatedLayers[i];
+            // Applies camera mouse offset
+            layer.transform.position = (Vector3)((Vector2)transform.position + new Vector2(cameraOffset.x * roomOffsetMax * i, cameraOffset.y * roomOffsetMax * i)) + new Vector3(0, 0, layer.transform.position.z);
+        }
+    }
+
+    public void SetCameraOffset(Vector3 offset)
+    {
+        cameraOffset = offset;
     }
 
     private void GenerateLayers()
@@ -49,9 +68,12 @@ public class RoomLayout : MonoBehaviour
         {
             instantiatedLayers.Add(Instantiate(roomLayers[i], transform.position + new Vector3(0, 0, layerOffset * i), Quaternion.identity, transform));
 
-            float color = (0 + i * .075f);
+            float color = (1 - i * .075f);
 
-            instantiatedLayers[instantiatedLayers.Count - 1].GetComponent<SpriteRenderer>().color = new Color(color,color,color,1);
+            GameObject layer = instantiatedLayers[instantiatedLayers.Count - 1];
+
+            layer.GetComponent<SpriteRenderer>().color = new Color(color,color,color,1);
+            layer.transform.localScale = Vector3.one - (Vector3.one * .025f * i);
         }
     }
 }
