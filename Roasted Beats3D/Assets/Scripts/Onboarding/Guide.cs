b9ded447170cbb681e8483fba08daa3f433c6cf0
Jstaria <joseph-staria@hotmail.com>
2024-10-28 11:52:20 -0400

    Dialogue working

--- Roasted Beats3D/Assets/Scripts/Onboarding/Guide.cs
+++ Roasted Beats3D/Assets/Scripts/Onboarding/Guide.cs
@@ -7,15 +7,30 @@ using UnityEngine;
 public class Guide : MonoBehaviour
 {
     [SerializeField] private List<DialogueSO> dialogues;
+    [SerializeField] private VoiceSO charVoice;
+
+    [SerializeField] private List<AudioSource> audioSources;
+    private int currentAudioSource;
+
     [SerializeField] private TextMeshPro TextMeshPro;
     [SerializeField] private GameObject Self;
 
+    [SerializeField] private float defaultTextSpeed = .05f;
+
+    [SerializeField] private float defaultPitch = 1;
+    [SerializeField] private float defaultPitchOffset = .1f;
+
     private string spokenText = "";
-    private int currentDialogueIndex = 0;
+    private int dialogueIndex = 0;
     private int currentDialogueListIndex = 0;
     private DialogueSO currentDialogueList;
 
     private Coroutine currentActive;
+
+    private void Start()
+    {
+        currentDialogueList = dialogues[0];
+    }
 
     private IEnumerator DialogueLoop(string text, float timeInbetween)
     {
@@ -26,6 +41,9 @@ public class Guide : MonoBehaviour
         for (int i = 0; i < textLength; i++)
         {
             spokenText += text[i];
+            TextMeshPro.text = spokenText;
+
+            Speak(charVoice.voiceList[Random.Range(0, charVoice.voiceList.Count)]);
 
             float newTime = timeInbetween + Random.Range(-timeInbetween / 4, timeInbetween / 4);
 
@@ -35,33 +53,50 @@ public class Guide : MonoBehaviour
         currentActive = null;
     }
 
+    private void Speak(AudioClip clip)
+    {
+        float pitch = defaultPitch + Random.Range(-defaultPitchOffset, defaultPitchOffset);
+
+        audioSources[currentAudioSource].clip = clip;
+        audioSources[currentAudioSource].pitch = pitch;
+        audioSources[currentAudioSource].Play();
+
+        currentAudioSource = (currentAudioSource + 1) % audioSources.Count;
+    }
+
     public void NextDialogueList()
     {
-        //currentDialogueList = dialogues[currentDialogueListIndex];
-        //currentDialogueListIndex++;
-        //currentDialogueListIndex = Mathf.Clamp(currentDialogueListIndex, 0, dialogues.Count - 1);
+        dialogueIndex = 0;
+
+        currentDialogueListIndex++;
+
+        currentDialogueListIndex = Mathf.Clamp(currentDialogueListIndex, 0, dialogues.Count - 1);
+
+        currentDialogueList = dialogues[currentDialogueListIndex];
     }
 
     public void NextDialogue()
     {
-        //DialogueObj temp = currentDialogueList.list[currentDialogueIndex];
+        DialogueObj temp = currentDialogueList.dialogueList[dialogueIndex];
 
-        //if (currentActive != null) StopCoroutine(currentActive);
+        if (currentActive != null) StopCoroutine(currentActive);
 
-        //currentActive = StartCoroutine(DialogueLoop(temp.text, temp.charWaitTime));
+        float waitTime = (temp.charWaitTime == -1) ? defaultTextSpeed : temp.charWaitTime;
 
-        //currentDialogueIndex++;
-        //currentDialogueIndex = Mathf.Clamp(currentDialogueIndex, 0, dialogues[currentDialogueListIndex - 1].list.Count - 1);
+        currentActive = StartCoroutine(DialogueLoop(temp.text, waitTime));
 
-        //Debug.Log(currentDialogueIndex);
+        dialogueIndex++;
+        dialogueIndex = Mathf.Clamp(dialogueIndex, 0, currentDialogueList.dialogueList.Count - 1);
+
+        Debug.Log(dialogueIndex);
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("pressed left");
-            NextDialogueList();
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("pressed left");
+        //    NextDialogueList();
+        //}
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("pressed right");
