fc60026d9d34643d071f5e59cca8fafbc336c761
Joseph <jws8554@rit.edu>
2024-11-05 11:51:22 -0500

    Added spring to select menu

--- Roasted Beats3D/Assets/Scripts/UI/MoveMenus.cs
+++ Roasted Beats3D/Assets/Scripts/UI/MoveMenus.cs
@@ -6,13 +6,34 @@ public class MoveMenus : MonoBehaviour
 {
     int moved = 0;
 
+    private Spring xSpring;
+
+    [SerializeField] float dampingRatio;
+    [SerializeField] float angularFrequency;
+    [SerializeField] float offsetDistance;
+
+    private void Start()
+    {
+        xSpring = new Spring(angularFrequency, dampingRatio, transform.position.x / offsetDistance, true);
+    }
+
+    private void Update()
+    {
+        xSpring.Update();
+
+        transform.position = new Vector3(xSpring.Position * offsetDistance, transform.position.y, transform.position.z);
+    }
+
     public void MoveRight()
     {
         if (moved < 2)
         {
-            Vector3 currentPos = transform.position;
-            currentPos.x -= 19.25f;
-            transform.position = currentPos;
+            //Vector3 currentPos = transform.position;
+            //currentPos.x -= 19.25f;
+            //transform.position = currentPos;
+
+            xSpring.RestPosition--;
+
             moved++;
         }
     }
@@ -21,9 +42,12 @@ public class MoveMenus : MonoBehaviour
     {
         if (moved > 0)
         {
-            Vector3 currentPos = transform.position;
-            currentPos.x += 19.25f;
-            transform.position = currentPos;
+            //Vector3 currentPos = transform.position;
+            //currentPos.x += 19.25f;
+            //transform.position = currentPos;
+
+            xSpring.RestPosition++;
+
             moved--;
         }
     }
